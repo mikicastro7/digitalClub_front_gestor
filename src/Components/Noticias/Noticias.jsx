d4ee7fc724ba3b8572ee4f04c030c5d3344da146
Miquel Castro <castro-miquel@hotmail.com>
2021-04-15 22:00:59 +0200

    Boton añadir noticia

--- src/Components/Noticias/Noticias.jsx
+++ src/Components/Noticias/Noticias.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Noticias = () => (
   <section>
     <h2>Noticias</h2>
+    <div className="add-noticia">
+      <Link to="/noticias/añadir-noticia">
+        Añadir noticia
+        {" "}
+        <FontAwesomeIcon icon={faPlus} />
+      </Link>
+    </div>
   </section>
 );
 
