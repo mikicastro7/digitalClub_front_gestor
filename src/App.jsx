a769c3b58d1a5ddf33bc72fcd5cadad0facca3f4
Miquel Castro <castro-miquel@hotmail.com>
2021-04-15 21:50:49 +0200

    Creación url noticias con componente noticias

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Components/UIcomponents/Navbar";
+import Noticias from "./Components/Noticias/Noticias";
 
 const App = () => (
   <>
     <Navbar />
+    <div className="container">
+      <Router>
+        <Switch>
+          <Route path="/noticias" exact>
+            <Noticias />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   </>
 );
 
---
+++ src/Components/Noticias/Noticias.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Noticias = () => (
+  <div>
+    <h1>Noticias</h1>
+  </div>
+);
+
+export default Noticias;
