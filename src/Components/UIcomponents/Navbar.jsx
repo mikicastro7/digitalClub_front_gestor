e95fc624f79969fbda98fb1c7126c75307e41fc8
mikicastro69 <castro-miquel@hotmail.com>
2021-04-17 13:03:20 +0200

    Links navbar añadidos

--- src/Components/UIcomponents/Navbar.jsx
+++ src/Components/UIcomponents/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NavbarComponent = () => (
   <Navbar bg="primary" variant="dark" expand="lg">
     <div className="container">
-      <Navbar.Brand className="nav-first" href="#home">Digital Clubs</Navbar.Brand>
+      <Navbar.Brand className="nav-first" as={Link} to="/">Digital Clubs</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <NavDropdown title="Gestionar" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.2">Noticias</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Equipos</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/noticias">Noticias</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/equipos">Equipos</NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link href="#link">Usuarios</Nav.Link>
+          <Nav.Link as={Link} to="/us">Usuarios</Nav.Link>
         </Nav>
         <Nav className="ml-auto nav-last">
           <NavDropdown title="Usuario" id="basic-nav-dropdown">
